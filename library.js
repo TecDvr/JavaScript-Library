d98f42b0d2793d45c102c7015a2e6b4b0b72fd88
Zach Gwirtz <zachgw@gmail.com>
2020-01-15 11:09:23 -0800

    parallax functinality

--- library.js
+++ library.js
@@ -1,24 +1,30 @@
 // object literal approach
+// add selector class tags
+// add translateX
 
 function zg(selector) {
     const self = {
-        element: document.querySelector(selector), // get element based off selector
-        html: () => self.element, // return selected element
+        element: document.querySelector(selector),
+        html: () => self.element,
         on: (event, callback) => {
             document.addEventListener(event, callback)
         },
         hide: () => {
             self.element.style.display = 'none'
         },
+        parallax: (speed, zindex, position) => {
+            position = (typeof position !== 'undefined') ?  position : 'relative'
+            self.element.style.cssText = `z-index: ${zindex}; position: ${position};`;
+            document.addEventListener('scroll', () => {
+                let scrollDistance = window.scrollY
+                self.element.style.transform = `translateY(${scrollDistance * speed}px)`;
+            });
+        }
     }
-
     return self
 }
 
-// console.log(zg('h1').html())
-
-// zg('h1').on('click', () => {
-//     alert('clicked')
-// })
-
-// zg('h1').hide()
+zg('header').parallax(1, -1);
+zg('.blue').parallax(0.1, 0, 'absolute')
+zg('.red').parallax(0.3, 0, 'absolute')
+zg('.yellow').parallax(0.6, 0, 'absolute')
